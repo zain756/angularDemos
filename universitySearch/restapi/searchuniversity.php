0055ba130e7f4541c273e4d97e4e470667fb6069
zain <zain.ul.abdeen113@gmail.com>
2016-11-19 23:36:05 +0500

    working on actual server

--- universitySearch/restapi/searchuniversity.php
+++ universitySearch/restapi/searchuniversity.php
@@ -1,10 +1,20 @@
 <?php
 // include Database connection file
 // Connection variables 
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+/*
+$host = "mysql6.000webhost.com"; // MySQL host name eg. localhost
+$user = "a4130694_user"; // MySQL user. eg. root ( if your on localserver)
+$password = "nokiax101"; // MySQL user password  (if password is not set for your root user then keep it empty )
+$database = "a4130694_db"; // MySQL Database name
+*/
+//use for testeing on localhost
 $host = "localhost"; // MySQL host name eg. localhost
 $user = "root"; // MySQL user. eg. root ( if your on localserver)
 $password = "zain"; // MySQL user password  (if password is not set for your root user then keep it empty )
 $database = "patientsdb"; // MySQL Database name
+
 // Connect to MySQL Database 
 $conn = mysqli_connect($host, $user, $password,$database) or die("Could not connect to database");
 
@@ -60,36 +70,6 @@ if (isset ($_GET['name']) || isset ($_GET['country'])|| isset ($_GET['domain'])
     
     $query = "SELECT * FROM universitytable WHERE country like '$country' and domain like '$domain' 
                and name like '$name' and alpha_two_code like '$alpha_two_code' and web_page like '$web_page' limit 20"  ;
-               /*
-    if (isset ($_GET['country']) && isset ($_GET['name']) && isset ($_GET['domain']) && isset ($_GET['web_page'])
-        && isset ($_GET['alpha_two_code'])){
-
-        $query = "SELECT * FROM universitytable WHERE country like '$country' and domain like '$domain' 
-               and name like '$name' and alpha_two_code like '$alpha_two_code' and web_page like '$web_page' limit 10"  ;
-    else if (!isset ($_GET['country']) && isset ($_GET['name']) && !isset ($_GET['domain']) && !isset ($_GET['web_page'])
-        && !isset ($_GET['alpha_two_code'])){
-
-        $query = "SELECT * FROM universitytable WHERE name like '$name' LIMIT 10"  ; 
-    }
-    else if (!isset ($_GET['country']) && !isset ($_GET['name']) && isset ($_GET['domain']) && !isset ($_GET['web_page'])
-        && !isset ($_GET['alpha_two_code'])){
-
-        $query = "SELECT * FROM universitytable WHERE domain like '$domain' LIMIT 10"  ; 
-    }
-    else if (!isset ($_GET['country']) && !isset ($_GET['name']) && !isset ($_GET['domain']) && isset ($_GET['web_page'])
-        && !isset ($_GET['alpha_two_code'])){
-
-        $query = "SELECT * FROM universitytable WHERE web_page like '$web_page' LIMIT 10"  ; 
-    }
-    else if (!isset ($_GET['country']) && !isset ($_GET['name']) && !isset ($_GET['domain']) && !isset ($_GET['web_page'])
-        && isset ($_GET['alpha_two_code'])){
-
-        $query = "SELECT * FROM universitytable WHERE alpha_two_code like '$alpha_two_code' LIMIT 10"  ; 
-    }
-    else{
-        $query = "SELECT * FROM universitytable WHERE country like 'U' LIMIT 10"  ; 
-    }
-    */
 
 
 

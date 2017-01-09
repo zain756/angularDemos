c29293ea008486401fec5593598e33a821e0e787
zain <zain.ul.abdeen113@gmail.com>
2017-01-10 00:23:45 +0500

    front end building tutorial with gulp

---
+++ simpleSite/gulpfile.js
@@ -0,0 +1,35 @@
+/// sudo npm install gulp --save-dev
+/// sudo npm install jshint gulp-jshint --save-dev
+/// sudo npm install browser-sync--save-dev
+
+var gulp = require ('gulp'),
+    jshint = require('gulp-jshint'),
+    //stylish = require('jshint-stylish'),
+	 browserSync = require('browser-sync');
+
+gulp.task('browser-sync',  function () {
+   var files = [
+      'app/**/*.html',
+      'app/css/**/*.css',
+      'app/images/**/*.png',
+      'app/scripts/**/*.js',
+      'build/**/*'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: "app",
+         index: "index.html"
+      }
+   });
+  // Watch any files in dist/, reload on change
+  gulp.watch(['app/**/*']).on('change', browserSync.reload);
+    });
+	
+
+gulp.task('jshint', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default', {beep: true}));
+});
+
